bfcae2ab22c153f99c6f4edbeafdd0e11d971d5f
Daniel Bell <djbell216@gmail.com>
2018-10-15 11:16:41 -0500

    Naming Convention Fix

--- HW4/HW4-Q1.sql
+++ HW4/HW4-Q1.sql
@@ -1,27 +1,27 @@
 --Q1
 create schema Homework4
 go
-use danieljbell;
 
+use danieljbell;
 create table Homework4.Club
 (
-	Clubid int NOT NULL identity(1, 1) primary key,
-	MemberName nvarchar(45) Not Null,
-	Purpose nvarchar(45) Not Null,
+	ClubId int NOT NULL identity(1, 1) primary key,
+	ClubName nvarchar(100) Not Null,
+	Purpose nvarchar(150) Not Null,
 	CreatedOn datetimeoffset Not Null,
 	UpdatedOn datetimeoffset Not Null
 	unique
 	(
-		MemberName	
+		ClubName	
 	)
 );
 
 create table Homework4.Meeting
 (
-	MeetingID int Not Null primary Key,
-	clubID int Not Null identity(1, 1) foreign key references Homework4.Club(ClubID),
+	MeetingID int Not Null identity(1, 1) primary Key,
+	clubID int Not Null foreign key references Homework4.Club(ClubID),
 	Meet_Time datetime2(0) Not Null,
-	Meet_Location nvarchar(45) Not Null,
+	Meet_Location nvarchar(100) Not Null,
 	CreatedOn datetimeoffset Not Null,
 	UpdatedOn datetimeoffset Not Null
 	unique
@@ -34,7 +34,7 @@ create table Homework4.Meeting
 create table Homework4.Attendee
 (
 	AttendeeID int Not Null identity(1, 1) primary key,
-	Email nvarchar(45) Not Null,
+	Email nvarchar(100) Not Null,
 	FirstName nvarchar(45) Not Null,
 	LastName nvarchar(45) Not Null,
 	CreatedOn datetimeoffset Not Null,
